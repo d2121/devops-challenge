59fa34bfc7896bf30838316dd20ffff02f619489
da <dm@m.com>
2023-11-15 22:07:46 +0200

    secret

--- verification.sh
+++ verification.sh
@@ -5,7 +5,15 @@ docker-compose up --build -d
 sleep 5
 
 # Query results
-curl 127.0.0.1:5000/secret | jq '.'
+
+response=$(curl -s 127.0.0.1:5000/secret)
+
+if [[ $? -eq 0 ]]; then
+    echo "$response" | jq '.'
+else
+    echo "Failed to fetch the secret."
+fi
+
 curl 127.0.0.1:5000/health | jq '.'
 
 # Destroy 
