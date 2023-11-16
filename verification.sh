0e744a58fba95f1c24264dad0acbb48c3fcfbcf2
da <dm@m.com>
2023-11-16 06:57:09 +0200

    print json as is

--- verification.sh
+++ verification.sh
@@ -6,12 +6,7 @@ sleep 5
 
 # Query results
 response=$(curl -s http://127.0.0.1:5000/secret)
-secret_code=$(echo $response | jq -r '.secret_code')
-if [ "$secret_code" == "null" ]; then
-    echo "secret_code = null"
-else
-    echo "secret_code = $secret_code"
-fi
+echo $response
 
 curl 127.0.0.1:5000/health | jq '.'
 
