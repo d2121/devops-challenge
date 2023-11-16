d7663fa0bc76f9b6e0ab611384be72b1bd7f870e
da <dm@m.com>
2023-11-16 06:52:30 +0200

    secret code

--- verification.sh
+++ verification.sh
@@ -5,7 +5,14 @@ docker-compose up --build -d
 sleep 5
 
 # Query results
-curl 127.0.0.1:5000/secret | jq -r '.secret_code'
+response=$(curl -s http://127.0.0.1:5000/secret)
+secret_code=$(echo $response | jq -r '.secret_code')
+if [ "$secret_code" == "null" ]; then
+    echo "secret_code = null"
+else
+    echo "secret_code = $secret_code"
+fi
+
 curl 127.0.0.1:5000/health | jq '.'
 
 # Destroy 
